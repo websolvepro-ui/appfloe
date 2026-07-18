35831b1dd16ce6db9b6f33ab7eefb9606d4c0cbf
websolvepro-ui <websolvepro@gmail.com>
2026-07-18 18:42:33 +0200

    Fix Cloud Run port

--- server.ts
+++ server.ts
@@ -8,7 +8,7 @@ import { GoogleGenAI, Type } from "@google/genai";
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.use(express.json());
 
